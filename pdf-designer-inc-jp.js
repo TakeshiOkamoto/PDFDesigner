189e9c9de1c160b1326d104f8841eb793a46a1ba
papy <e-papy@nifty.com>
2016-01-05 23:21:58 +0900

    Create pdf-designer-inc-jp.js

---
+++ pdf-designer-inc-jp.js
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////
+//  開発時用のエラーメッセージです。
+////////////////////////////////////////////////////////////////////////////////
+// The error message in English, please read the "pdf-designer-inc-en.js"
+
+// TPDFAnalyst
+var PDFDESIGNER_ERROR_001 = 'ストリームがありません。';
+var PDFDESIGNER_ERROR_002 = 'Indexの指定が範囲を超えています。';
+var PDFDESIGNER_ERROR_003 = '未実装';
+var PDFDESIGNER_ERROR_004 = 'オブジェクトIDが不正です。';
+var PDFDESIGNER_ERROR_005 = 'ストリームが不正です。';
+var PDFDESIGNER_ERROR_006 = 'FirstPagesが存在しません。';
+var PDFDESIGNER_ERROR_007 = '相互参照テーブルの位置が不正です。';
+var PDFDESIGNER_ERROR_008 = '相互参照テーブルの先頭オブジェクト番号が不正です。';
+var PDFDESIGNER_ERROR_009 = '相互参照テーブルの項目数が不正です。';
+var PDFDESIGNER_ERROR_010 = 'trailerが正しくありません。';
+var PDFDESIGNER_ERROR_011 = 'RootIDの取得に失敗しました。';
+var PDFDESIGNER_ERROR_012 = 'PrevIDの取得に失敗しました。';
+var PDFDESIGNER_ERROR_013 = 'InfoIDの取得に失敗しました。';
+var PDFDESIGNER_ERROR_014 = 'ファイルが壊れています。';
+var PDFDESIGNER_ERROR_015 = 'PDFファイルではありません。';
+var PDFDESIGNER_ERROR_016 = '%%EOFがありません。';
+var PDFDESIGNER_ERROR_017 = '相互参照テーブルの位置を正確に取得できませんでした。';
+var PDFDESIGNER_ERROR_018 = 'RootIDが正確に取得できませんでした。';
+var PDFDESIGNER_ERROR_019 = 'FirstPagesIDが正確に取得できませんでした。';
+var PDFDESIGNER_ERROR_020 = '(予約)';
+var PDFDESIGNER_ERROR_021 = '(予約)';
+var PDFDESIGNER_ERROR_022 = '(予約)';
+var PDFDESIGNER_ERROR_023 = '(予約)';
+var PDFDESIGNER_ERROR_024 = '(予約)';
+
+// all class
+var PDFDESIGNER_ERROR_025 = '暗号化されたファイルには対応していません。';
+var PDFDESIGNER_ERROR_026 = '(予約)';
+var PDFDESIGNER_ERROR_027 = '(予約)';
+var PDFDESIGNER_ERROR_028 = '(予約)';
+var PDFDESIGNER_ERROR_029 = '(予約)';
+
+// TPDFKnife,TPDFDeletePage,TPDFRotatePage,TPDFOutLineManager
+var PDFDESIGNER_ERROR_030 = 'ページの指定が範囲を超えています。';
+var PDFDESIGNER_ERROR_031 = '(予約)';
+var PDFDESIGNER_ERROR_032 = '(予約)';
+var PDFDESIGNER_ERROR_033 = '(予約)';
+var PDFDESIGNER_ERROR_034 = '(予約)';
+
+// TPDFDeletePage
+var PDFDESIGNER_ERROR_035 = '全てのページを削除する事は出来ません。';
+var PDFDESIGNER_ERROR_036 = '(予約)';
+var PDFDESIGNER_ERROR_037 = '(予約)';
+var PDFDESIGNER_ERROR_038 = '(予約)';
+var PDFDESIGNER_ERROR_039 = '(予約)';
+
+// TPDFRotatePage
+var PDFDESIGNER_ERROR_040 = '第三引数のRotateには1-3を設定してください。';
+var PDFDESIGNER_ERROR_041 = '(予約)';
+var PDFDESIGNER_ERROR_042 = '(予約)';
+var PDFDESIGNER_ERROR_043 = '(予約)';
+var PDFDESIGNER_ERROR_044 = '(予約)';
+
+// TPDFCode
+var PDFDESIGNER_ERROR_045 = 'Unicodeの文字列が途中で途切れています。';
+var PDFDESIGNER_ERROR_046 = '(予約)';
+var PDFDESIGNER_ERROR_047 = '(予約)';
+var PDFDESIGNER_ERROR_048 = '(予約)';
+var PDFDESIGNER_ERROR_049 = '(予約)';
+
+// TPDFNode,TPDFNodeList,TPDFOutLineManager
+var PDFDESIGNER_ERROR_050 = '子ノードは作成されていません。';
+var PDFDESIGNER_ERROR_051 = 'Indexの指定が不正です。';
+var PDFDESIGNER_ERROR_052 = 'しおりの階層レベルは5段階までです。これ以上、子ノードを追加できません。';
+var PDFDESIGNER_ERROR_053 = 'ノードは作成されていません。';
+var PDFDESIGNER_ERROR_054 = '(予約)';
