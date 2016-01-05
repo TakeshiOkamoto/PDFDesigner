ee3cfb15e35c0b10cb47cbdd0bed85fe13dcfcc5
papy <e-papy@nifty.com>
2016-01-05 23:21:35 +0900

    Create pdf-designer-inc-en.js

---
+++ pdf-designer-inc-en.js
@@ -0,0 +1,71 @@
+////////////////////////////////////////////////////////////////////////////////
+// This is the error message for the time of development.
+////////////////////////////////////////////////////////////////////////////////
+
+// TPDFAnalyst
+var PDFDESIGNER_ERROR_001 = 'There is no stream.';
+var PDFDESIGNER_ERROR_002 = 'Index out of range.';
+var PDFDESIGNER_ERROR_003 = 'Unimplemented';
+var PDFDESIGNER_ERROR_004 = 'ObjectID is invalid.';
+var PDFDESIGNER_ERROR_005 = 'Stream is invalid.';
+var PDFDESIGNER_ERROR_006 = 'FirstPage does not exist.';
+var PDFDESIGNER_ERROR_007 = 'The position of the cross-reference table is invalid.';
+var PDFDESIGNER_ERROR_008 = 'Top object number of the cross-reference table is invalid.';
+var PDFDESIGNER_ERROR_009 = 'Number of items in the cross-reference table is invalid.';
+var PDFDESIGNER_ERROR_010 = 'Trailer is not correct.';
+var PDFDESIGNER_ERROR_011 = 'It failed to get RootID.';
+var PDFDESIGNER_ERROR_012 = 'It failed to get PrevID.';
+var PDFDESIGNER_ERROR_013 = 'It failed to get InfoID.';
+var PDFDESIGNER_ERROR_014 = 'File is corrupted.';
+var PDFDESIGNER_ERROR_015 = 'Not a PDF file.';
+var PDFDESIGNER_ERROR_016 = '%%EOF is not.';
+var PDFDESIGNER_ERROR_017 = 'The position of the cross-reference table could not be accurately obtained.';
+var PDFDESIGNER_ERROR_018 = 'RootID could not be obtained accurately.';
+var PDFDESIGNER_ERROR_019 = 'FirstPagebID could not be obtained accurately.';
+var PDFDESIGNER_ERROR_020 = '(Reservation)';
+var PDFDESIGNER_ERROR_021 = '(Reservation)';
+var PDFDESIGNER_ERROR_022 = '(Reservation)';
+var PDFDESIGNER_ERROR_023 = '(Reservation)';
+var PDFDESIGNER_ERROR_024 = '(Reservation)';
+
+// all class
+var PDFDESIGNER_ERROR_025 = 'It does not support encrypted files.';
+var PDFDESIGNER_ERROR_026 = '(Reservation)';
+var PDFDESIGNER_ERROR_027 = '(Reservation)';
+var PDFDESIGNER_ERROR_028 = '(Reservation)';
+var PDFDESIGNER_ERROR_029 = '(Reservation)';
+
+// TPDFKnife,TPDFDeletePage,TPDFRotatePage,TPDFOutLineManager
+var PDFDESIGNER_ERROR_030 = 'The page is out of range.';
+var PDFDESIGNER_ERROR_031 = '(Reservation)';
+var PDFDESIGNER_ERROR_032 = '(Reservation)';
+var PDFDESIGNER_ERROR_033 = '(Reservation)';
+var PDFDESIGNER_ERROR_034 = '(Reservation)';
+
+// TPDFDeletePage
+var PDFDESIGNER_ERROR_035 = 'It is not possible to remove all of the page.';
+var PDFDESIGNER_ERROR_036 = '(Reservation)';
+var PDFDESIGNER_ERROR_037 = '(Reservation)';
+var PDFDESIGNER_ERROR_038 = '(Reservation)';
+var PDFDESIGNER_ERROR_039 = '(Reservation)';
+
+// TPDFRotatePage
+var PDFDESIGNER_ERROR_040 = 'Please set 1-3 to Rotate the third argument.';
+var PDFDESIGNER_ERROR_041 = '(Reservation)';
+var PDFDESIGNER_ERROR_042 = '(Reservation)';
+var PDFDESIGNER_ERROR_043 = '(Reservation)';
+var PDFDESIGNER_ERROR_044 = '(Reservation)';
+
+// TPDFCode
+var PDFDESIGNER_ERROR_045 = 'Unicode strings are interrupted in the middle.';
+var PDFDESIGNER_ERROR_046 = '(Reservation)';
+var PDFDESIGNER_ERROR_047 = '(Reservation)';
+var PDFDESIGNER_ERROR_048 = '(Reservation)';
+var PDFDESIGNER_ERROR_049 = '(Reservation)';
+
+// TPDFNode,TPDFNodeList,TPDFOutLineManager
+var PDFDESIGNER_ERROR_050 = 'Child node is not created.';
+var PDFDESIGNER_ERROR_051 = 'Index is invalid.';
+var PDFDESIGNER_ERROR_052 = 'Hierarchy level of bookmark is up to five levels. No more, you can not add a child node.';
+var PDFDESIGNER_ERROR_053 = 'Node is not created.';
+var PDFDESIGNER_ERROR_054 = '(Reservation)';
